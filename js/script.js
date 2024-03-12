55f60e3cffcbec4a46f184416bd3490244ff3dd9
Lombax <120056784+Lombax-dev@users.noreply.github.com>
2024-03-12 16:23:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Etudiant', 'YouTuber/Streamer', 'Responsable Serveur'],
+    strings: ['Etudiant', 'Apprenti Développeur', 'Responsable Serveur'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
